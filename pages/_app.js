aca407bf16665a0aa680af3ea5014d64ac717374
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-09-11 01:05:51 -0400

    og-data

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,11 @@ function MyApp({ Component, pageProps }) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/devicon/2.14.0/devicon.min.css" integrity="sha512-Fx1qTIVtFTb41Tqu+TxfaaPCcpmkRIbOIKh+4OIwVYAECoW89rz4BnRy95Vu8MYSRgghC3pS9mJ435hzarnZcw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta name="theme-color" content="#3d3d3d" />
+      <meta name="theme-color" content="#00c8ff" />
+      <meta property="og:site_name" content="Next.js-tailwind" />
+      <meta property="og:title" content="og:title" />
+      <meta property="og:description" content="og:description" />
+      <meta property="og:image" content="/og-image.jpeg" />
     </Head>
     <Component {...pageProps} />
   </>
