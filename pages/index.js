647ae4f8599b19fdd3974929825a025f165c7122
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-09-11 19:05:36 -0400

    darkmode finished

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,12 @@ export default function Home() {
         <i className="devicon-nextjs-plain-wordmark colored cursor-pointer" />
       </div>
       <div className="select-none mr-5 sm:mr-10 flex justify-center items-center text-sm sm:text-xl">
-        <div className="bg-white dark:bg-gray-300 rounded-full shadow-2xl cursor-pointer ring-1 ring-gray-100 hover:ring-green-400 dark:ring-gray-300 dark:hover:ring-green-400 hover:bg-green-400 dark:hover:bg-green-400  transition duration-100 ease-in-out">
-          <i className="fas fa-shopping-cart p-3" />
+        <div className="h-10 w-10 flex justify-center items-center bg-white dark:text-gray-200 dark:hover:text-black dark:bg-gray-800 rounded-full shadow-2xl cursor-pointer ring-1 ring-gray-100 hover:ring-green-400 dark:ring-gray-800 dark:hover:ring-green-400 hover:bg-green-400 dark:hover:bg-green-400 transition duration-100 ease-in-out">
+          <i className="fas fa-shopping-cart" />
         </div>
       </div>
     </div>
-    <div className="container mb-5 bg-gradient-to-r from-white via-pink-100 to-pink-200 dark:from-gray-200 dark:via-purple-300 dark:to-purple-500 sm:rounded-2xl">
+    <div className="container mb-5 py-5 pl-5 bg-gradient-to-r from-white via-pink-100 to-pink-200 dark:from-gray-200 dark:via-purple-300 dark:to-purple-500 sm:rounded-2xl">
       <div className="text-7xl max-w-min flex items-center">
         <div className="animate-pulse flex">
           <i className="devicon-nodejs-plain colored"/>
@@ -43,7 +43,7 @@ export default function Home() {
         className="cursor-pointer select-none h-8 w-16 rounded-full shadow-md ring-2 ring-gray-200 dark:ring-gray-400  bg-gradient-to-tr from-white to-gray-100 dark:from-gray-200 dark:to-gray-400 transition duration-150"
       >
         <div className={`h-8 w-8 p-1 transition-all duration-150 ease-in-out dark:ml-8`}>
-          <div className="h-full rounded-full bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 dark:from-purple-400 dark:to-pink-500" />
+          <div className="h-full rounded-full bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 dark:from-blue-400 dark:via-purple-600 dark:to-gray-600" />
         </div>
       </div>
     </div>
