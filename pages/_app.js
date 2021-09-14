8f37151b15ac7f5a6f79999589efcaf8deced9de
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-09-14 01:31:02 -0400

    3.1.4.1

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/devicon/2.14.0/devicon.min.css" integrity="sha512-Fx1qTIVtFTb41Tqu+TxfaaPCcpmkRIbOIKh+4OIwVYAECoW89rz4BnRy95Vu8MYSRgghC3pS9mJ435hzarnZcw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no" />
-      <meta name="theme-color" content={darkMode ? "#7d54ed" : "#f56d51"} />
+      <meta name="theme-color" content={darkMode ? "#7d54ed" : "#f33984"} />
       <meta property="og:site_name" content="Next.js-tailwind" />
       <meta property="og:title" content="og:title" />
       <meta property="og:description" content="og:description" />
